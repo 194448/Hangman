b5b7fdda5e393134ba6aa1a53506a0fcd8b0fdf4
194448 <60668129+194448@users.noreply.github.com>
2020-05-15 23:49:21 -0400

    2/3 done
    added new game function, guessing, and buttons to add letters.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,13 +22,14 @@ namespace Hangman
     {
         string[] Words = new string[10];
         string Word2Guess = "";
+        string tempword = "";
         Random rnd = new Random();
-
+        Button[] btnLetters;
+        int BadGuess = 0;
         public MainWindow()
         {
             InitializeComponent();
-
-
+            addButtons();
             System.IO.StreamReader sr = new System.IO.StreamReader("Words.txt");
             int counter = 0;
             int num = rnd.Next(1, 10);
@@ -37,13 +38,119 @@ namespace Hangman
                 Words[counter] = sr.ReadLine();
                 if (counter == num)
                 {
-                    MessageBox.Show(Words[counter]);
-                    WordHint.Text = Words[counter].ToString();
+                    //MessageBox.Show(Words[counter]);
+                    //WordHint.Text = Words[counter].ToString();
+                    string Word2Guess = Words[counter].ToString();
+                    //WordHint.Text = Word2Guess.ToString();
+                    lblWord2Guess.Content = Word2Guess.ToString();
+                    for (int i = 0; i < Word2Guess.Length; i++)
+                    {
+                        WordHint.Text += "_ ";
+                    }
                 }
                 
                 counter++;
             }
-            sr.Close(); 
+            sr.Close();
+            Word2Guess = lblWord2Guess.Content.ToString();
+           
+
+        }
+
+        private void delButtons()
+        {
+            LetterLocation.Children.Clear();
+        }
+
+        private void addButtons()
+        {
+            btnLetters = new Button[26];
+            int A = (int)'A';
+            for (int i = 0; i < 26; i++) 
+            {
+                btnLetters[i] = new Button() { Content = ((char)(i + A)).ToString(), Width = 40 };
+                btnLetters[i].Click += btnGuessLetters;
+                LetterLocation.Children.Add(btnLetters[i]);
+            }
+        }
+
+        private void btnGuessLetters(object sender, RoutedEventArgs e)
+        {
+            Button temp = (Button)sender;
+            string Letter2Guess = temp.Content.ToString();
+            for (int i = 0; i < Word2Guess.Length; i++)
+            {
+                if (Letter2Guess == Word2Guess[i].ToString().ToUpper())
+                {
+                    tempword += Word2Guess[i] + " ";
+                }
+
+                else
+                {
+                    tempword += WordHint.Text.ToString()[i * 2] + " ";
+                }
+            }
+            if (tempword == WordHint.Text)
+            {
+                BadGuess++;
+            }
+
+            //MessageBox.Show(tempword.ToString());
+            WordHint.Text = tempword;
+            tempword = "";
+            //MessageBox.Show(temp.Content.ToString());
+            temp.Visibility = Visibility.Hidden;
+        }
+
+        private void btnGuess_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtbxInput.Text.ToUpper() == lblWord2Guess.Content.ToString().ToUpper())
+            {
+                lblWin.Visibility = Visibility.Visible;
+            }
+
+            else
+            {
+                BadGuess++;
+            }
+        }
+
+        private void btnReset_Click(object sender, RoutedEventArgs e)
+        {
+            string[] Words = new string[10];
+            string tempword = "";
+            int BadGuess = 0;
+
+
+            System.IO.StreamReader sr = new System.IO.StreamReader("Words.txt");
+            delButtons();
+            int counter = 0;
+            int num = rnd.Next(1, 10);
+            WordHint.Text = "";
+            lblWin.Visibility = Visibility.Collapsed;
+            while (!sr.EndOfStream)
+            {
+                Words[counter] = sr.ReadLine();
+                if (counter == num)
+                {
+                    //MessageBox.Show(Words[counter]);
+                    //WordHint.Text = Words[counter].ToString();
+                    string Word2Guess = Words[counter].ToString();
+                    //WordHint.Text = Word2Guess.ToString();
+                    lblWord2Guess.Content = Word2Guess.ToString();
+                    for (int i = 0; i < Word2Guess.Length; i++)
+                    {
+                        WordHint.Text += "_ ";
+                    }
+                }
+
+                counter++;
+            }
+            sr.Close();
+            Word2Guess = lblWord2Guess.Content.ToString();
+            txtbxInput.Text = "";
+            addButtons();
+
         }
     }
 }
